4fc1de7af98f601907976c7e4f73fb52cb9988d1
Christian Estela <cestela3@gmail.com>
2021-09-21 17:08:56 -0400

    Initial commit

---
+++ src/com/company/MultTable.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class MultTable {
+    public static void main(String[] args){
+        for (int a=1; a<=12; a++){
+            for(int b=1; b<=12; b++){
+                System.out.print((a*b)+"\t");
+            }
+            System.out.println();
+        }
+    }
+}
